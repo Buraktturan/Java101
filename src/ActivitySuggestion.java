2a4d678395731b19dbffdc30601687d73037e719
Burak Turan <buraktturann@gmail.com>
2024-01-22 22:40:23 +0300

    Aktivite Öneri Programı tamamlandı.

---
+++ src/ActivitySuggestion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ActivitySuggestion {
+    public static void main(String[] args) {
+        double heat;
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please Enter the Air Temperature");
+        heat=scan.nextDouble();
+        scan.close();
+        
+        if(heat<5){
+            System.out.println("You should go skiing");
+        }else if (5<=heat && heat<=15){
+            System.out.println("You should go cinema");
+        }else if (15<heat && heat<=25){
+            System.out.println("You should go picnic");
+        }else {
+            System.out.println("You should go swimming");
+        }
+    }
+}
